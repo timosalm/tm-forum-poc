ce5b98e90bd55de83d8b8eca79ba117ec2c7dccb
Timo Salm <tsalm@vmware.com>
2022-05-10 12:44:58 +0200

    Add additional content

--- workshop/workshop/setup.d/02-vscode.sh
+++ workshop/workshop/setup.d/02-vscode.sh
@@ -2,4 +2,4 @@
 set -x
 set +e
 
-jq ". + { \"java.server.launchMode\": \"Standard\", \"tanzu.sourceImage\": \"${CONTAINER_REGISTRY_HOSTNAME}/tap-wkld/spring-sensors-source\", \"tanzu.namespace\": \"${SESSION_NAMESPACE}\", \"redhat.telemetry.enabled\": false }" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
+jq ". + { \"java.server.launchMode\": \"Standard\", \"tanzu.sourceImage\": \"${CONTAINER_REGISTRY_HOSTNAME}/${CONTAINER_REGISTRY_REPOSITORY}/spring-sensors-source\", \"tanzu.namespace\": \"${SESSION_NAMESPACE}\", \"redhat.telemetry.enabled\": false }" /home/eduk8s/.local/share/code-server/User/settings.json | sponge /home/eduk8s/.local/share/code-server/User/settings.json
